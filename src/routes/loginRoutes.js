585bd5933e46d70cc0629dbba157274ca1a077bd
Danilo Martins <dansm86@outlook.com>
2022-10-04 18:35:59 -0300

    req04- completed
    Added validations to /login endpoint

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -7,8 +7,40 @@ const router = express.Router();
 
 router.use(bodyParser.json());
 
-router.post('/', (_req, res) => {
+function convertErrorMessage(errorMessage) {
+  if (errorMessage.search(/"email" is required/i) >= 0) {
+    return 'O campo "email" é obrigatório';
+  }
+
+  if (errorMessage.search(/"email" must be/i) >= 0) {
+    return 'O "email" deve ter o formato "email@email.com"';
+  }
+
+  if (errorMessage.search(/"password" is required/i) >= 0) {
+    return 'O campo "password" é obrigatório';
+  }
+
+  if (errorMessage.search(/"password" length/i) >= 0) {
+    return 'O "password" deve ter pelo menos 6 caracteres';
+  }
+}
+
+router.post('/', (req, res) => {
   const token = crypto.randomBytes(8).toString('hex');
+  const schema = Joi.object().keys({
+    email: Joi.string().trim().email()
+            .required(),
+    password: Joi.string().min(6).required(),
+  });
+
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    console.log(error.message);
+    const message = convertErrorMessage(error.message);
+    console.log(message);
+    return res.status(400).json({ message });
+  }
   res.status(200).json({ token });
 });
 
